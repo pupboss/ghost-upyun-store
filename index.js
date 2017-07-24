cea292f6f8311b79997024daab10d0e6b719fd30
Jet Lee <pupboss.i@gmail.com>
2017-07-24 16:22:56 +0800

    First commit

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-// # Upyun storage module for Ghost blog
+'use strict';
 
-var path = require('path');
-    fs  = require('fs-extra');
-    Promise = require('bluebird');
-    UPYUN = require('upyun');
-    moment = require('moment');
-	util = require('util');
-	BaseStore = require('../../../core/server/storage/base');
+var Promise = require('bluebird');
+var UPYUN = require('upyun');
+var moment = require('moment');
+var util = require('util');
+var BaseAdapter = require('ghost-storage-base');
 
 
-function UpyunStore(config) {
-	BaseStore.call(this);
+function UpyunAdapter(config) {
+	BaseAdapter.call(this);
 	this.options = config || {};
 }
 
-util.inherits(UpyunStore, BaseStore);
+util.inherits(UpyunAdapter, BaseAdapter);
 
 // ### Save
 // Saves the image to Upyun
 // - image is the express image object
 // - returns a promise which ultimately returns the full url to the uploaded image
-UpyunStore.prototype.save = function(image) {
+UpyunAdapter.prototype.save = function(image) {
 	var _this = this;
 
 	var upyun = new UPYUN (this.options.bucket, this.options.operator, this.options.password, 'v0.api.upyun.com', {apiVersion: 'v2'});
@@ -29,11 +27,11 @@ UpyunStore.prototype.save = function(image) {
 		var remotePath = _this.getRemotePath(image);
 		var remoteURL = _this.options.domain;
 		upyun.putFile(remotePath, image.path, null, false, {}, function (err, result) {
-			if (err || result.statusCode!=200) {
+			if (err || result.statusCode != 200) {
 				reject('[' + result.data.code + '] ' + result.data.msg);
 			} else {
-				if (_this.options.imgVersion != undefined) {
-					resolve(remoteURL + remotePath + _this.options.imgVersion);
+				if (_this.options.imageVersion != undefined) {
+					resolve(remoteURL + remotePath + _this.options.imageVersion);
 				} else {
 					resolve(remoteURL + remotePath);
 				}
@@ -43,28 +41,28 @@ UpyunStore.prototype.save = function(image) {
 };
 
 // middleware for serving the files
-UpyunStore.prototype.serve = function () {
+UpyunAdapter.prototype.serve = function () {
 	// a no-op, these are absolute URLs
 	return function (req, res, next) {
 		next();
 	};
 };
 
-UpyunStore.prototype.exists = function () {
+UpyunAdapter.prototype.exists = function () {
 	// Server side will automatically replace the file.
 	return;
 };
 
-UpyunStore.prototype.delete = function (target) {
+UpyunAdapter.prototype.delete = function (target) {
 	//For backup and security purposes there is no way to delete files
 	//whatever on local or server side through Ghost, please do it manually.
 	return;
 };
 
-UpyunStore.prototype.getRemotePath = function (image) {
-	var prefix = moment().format(this.options.filePath || 'YYYY/MM/').replace(/^\//, '');
+UpyunAdapter.prototype.getRemotePath = function (image) {
+	var folder = moment().format(this.options.folder || 'YYYY/MM/').replace(/^\//, '');
 
-	return '/' + prefix + image.name;
+	return '/' + this.options.prefix + folder + image.name;
 };
 
-module.exports = UpyunStore;
+module.exports = UpyunAdapter;
